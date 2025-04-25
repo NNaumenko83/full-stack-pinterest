5a5a223f139e2cfd26bacf369d9424ee17cdd75d
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-04-25 14:06:13 +0300

    [Add] postInteractions

--- src/components/postInteracions/postInteractions.jsx
+++ src/components/postInteracions/postInteractions.jsx
@@ -1,7 +1,18 @@
 import "./postInteractions.css";
+import Image from "../image/image";
 
 const PostInteractions = () => {
-  return <div>Posy</div>;
+  return (
+    <div className="postInteractions">
+      <div className="interactionIcons">
+        <Image path="/general/react.svg" alt="pic" />
+        273
+        <Image path="/general/share.svg" alt="pic" />
+        <Image path="/general/more.svg" alt="pic" />
+      </div>
+      <button type="button">Save</button>
+    </div>
+  );
 };
 
 export default PostInteractions;
