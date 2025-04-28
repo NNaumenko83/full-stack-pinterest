4526a47c56e57bf681267a0b7d6747a5e677a835
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-04-28 09:23:12 +0300

    [Add] Emoji picker

--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,7 +1,12 @@
 import "./comments.css";
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 import Image from "../image/image";
 
 const Comments = () => {
+  const [open, setOpen] = useState(false);
+
+  useState;
   return (
     <div className="comments">
       <div className="commentList">
@@ -57,7 +62,13 @@ const Comments = () => {
       <form className="commentForm">
         <input type="text" name="" id="" placeholder="Add a comment" />
         <div className="emoji">
-          <div className="">😊</div>
+          {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+          <div onClick={() => setOpen((prev) => !prev)}>😊</div>
+          {open && (
+            <div className="emojiPicker">
+              <EmojiPicker />
+            </div>
+          )}
         </div>
       </form>
     </div>
