769c8340c1f456b64a35c5ceaf2be2b117f3eb42
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-07-08 22:09:31 +0300

    feat: enhance Workspace component with mouse event handling for text layer manipulation

--- src/components/editor/Workspace.jsx
+++ src/components/editor/Workspace.jsx
@@ -1,10 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import useEditorStore from "../../utils/editorStore";
 import Image from "../image/image";
 
 export const Workspace = ({ previewImg }) => {
-  const { textOptions, setTextOptions, canvasOptions, setCanvasOptions } =
-    useEditorStore();
+  const {
+    textOptions,
+    setTextOptions,
+    canvasOptions,
+    setCanvasOptions,
+    setSelectedLayer,
+  } = useEditorStore();
+
+  const itemRef = useRef(null);
+  const containerRef = useRef(null);
+  const dragging = useRef(false);
+  const offset = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     if (canvasOptions.height === 0) {
@@ -18,6 +28,31 @@ export const Workspace = ({ previewImg }) => {
     }
   }, [previewImg, canvasOptions, setCanvasOptions]);
 
+  const handleMouseMove = (e) => {
+    if (!dragging.current) return;
+    setTextOptions({
+      ...textOptions,
+      left: e.clientX - offset.current.x,
+      top: e.clientY - offset.current.y,
+    });
+  };
+  const handleMouseUp = (e) => {
+    dragging.current = false;
+  };
+  const handleMouseLeave = (e) => {
+    dragging.current = false;
+  };
+  const handleMouseDown = (e) => {
+    setSelectedLayer("text");
+    dragging.current = true;
+    console.log("e.clientX:", e.clientX);
+    console.log("textOptions.left:", textOptions.left);
+    offset.current = {
+      x: e.clientX - textOptions.left,
+      y: e.clientY - textOptions.top,
+    };
+  };
+
   return (
     <div className="workspace">
       <div
@@ -26,6 +61,10 @@ export const Workspace = ({ previewImg }) => {
           height: canvasOptions.height,
           backgroundColor: canvasOptions.backgroundColor,
         }}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+        ref={containerRef}
       >
         <img src={previewImg.url} alt="" />
         {textOptions.text && (
@@ -38,6 +77,8 @@ export const Workspace = ({ previewImg }) => {
                 fontSize: `${textOptions.fontSize}px`,
                 color: textOptions.color,
               }}
+              onMouseDown={handleMouseDown}
+              ref={itemRef}
             >
               <input
                 value={textOptions.text}
